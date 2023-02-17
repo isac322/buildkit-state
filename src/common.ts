203575bfbb99671dd98e6a5c835dfba77aafe3f1
Byeonghoon Yoo <bhyoo@bhyoo.com>
2023-02-18 00:50:48 +0900

    chore: rebase onto actions/typescript-action (#10)
    * chore: rebase
    
    * ci(gha): rollback release ci
    
    * chore: build on docker
    
    * ci(gha): 🚧
    
    * ci(gha): 🚧
    
    * ci(gha): 🚧
    
    * ci(gha): typo
    
    * ci(gha): typo
    
    * ci(gha): typo

--- src/common.ts
+++ src/common.ts
@@ -1,22 +1,22 @@
-export const BUILDKIT_STATE_PATH = '/tmp/buildkit-cache/buildkit-state.tar';
-export const STATE_RESTORED_CACHE_KEY = 'restored-cache-key';
+export const BUILDKIT_STATE_PATH = '/tmp/buildkit-cache/buildkit-state.tar'
+export const STATE_RESTORED_CACHE_KEY = 'restored-cache-key'
 export const STATE_TYPES = [
   'regular',
   'source.local',
   'exec.cachemount',
   'frontend',
   'internal'
-];
+]
 
 export interface Inputs {
-  buildxName: string;
-  buildxContainerName: string;
+  buildxName: string
+  buildxContainerName: string
 }
 
 export function getContainerName(opts: Inputs): string {
-  if (opts.buildxContainerName != '') {
-    return opts.buildxContainerName;
+  if (opts.buildxContainerName !== '') {
+    return opts.buildxContainerName
   } else {
-    return `buildx_buildkit_${opts.buildxName}0`;
+    return `buildx_buildkit_${opts.buildxName}0`
   }
 }
