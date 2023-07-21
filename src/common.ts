d7bbe8039c93744e53cbf553c5d1fcb5d93c1e80
Byeonghoon Yoo <bhyoo@bhyoo.com>
2023-07-22 02:23:49 +0900

    feat: rewrite ts

--- src/common.ts
+++ src/common.ts
@@ -1,22 +1,58 @@
-export const BUILDKIT_STATE_PATH = '/tmp/buildkit-cache/buildkit-state.tar'
-export const STATE_RESTORED_CACHE_KEY = 'restored-cache-key'
-export const STATE_TYPES = [
-  'regular',
-  'source.local',
-  'exec.cachemount',
-  'frontend',
-  'internal'
-]
+import os from 'os'
+import * as core from '@actions/core'
+import * as toolCache from '@actions/tool-cache'
 
-export interface Inputs {
-  buildxName: string
-  buildxContainerName: string
+const binaryPrefix = 'buildkit-state'
+export const binaryName = 'buildkit_state'
+
+export async function getBinary(version: string): Promise<string> {
+  const cachedPath = toolCache.find(binaryName, version)
+  if (cachedPath) {
+    core.info('Restore from cache')
+    return cachedPath
+  }
+
+  const filename = getFilename()
+  core.debug(`filename: ${filename}`)
+
+  core.info(`Downloading ${filename}...`)
+  const downPath = await toolCache.downloadTool(
+    `https://github.com/isac322/buildkit-state/releases/download/v${version}/${filename}`
+  )
+  core.info(`Caching ${filename} for future usage...`)
+  return await toolCache.cacheFile(downPath, filename, binaryName, version)
 }
 
-export function getContainerName(opts: Inputs): string {
-  if (opts.buildxContainerName !== '') {
-    return opts.buildxContainerName
-  } else {
-    return `buildx_buildkit_${opts.buildxName}0`
+function getFilename(): string {
+  const platform = os.platform()
+  const arch = os.arch()
+
+  switch (platform) {
+    case 'darwin':
+      switch (arch) {
+        case 'arm64':
+          return `${binaryPrefix}-${platform}-arm64`
+        case 'x64':
+          return `${binaryPrefix}-${platform}-amd64`
+      }
+      break
+    case 'linux':
+      switch (arch) {
+        case 'arm':
+          return `${binaryPrefix}-${platform}-arm-5`
+        case 'arm64':
+          return `${binaryPrefix}-${platform}-arm64`
+        case 'x64':
+          return `${binaryPrefix}-${platform}-amd64`
+      }
+      break
+    case 'win32':
+      switch (arch) {
+        case 'x64':
+          return `${binaryPrefix}-windows-amd64.exe`
+      }
   }
+  throw new Error(
+    `Unsupported platform (${platform}) and architecture (${arch})`
+  )
 }
--- src/post.ts
+++ src/post.ts
@@ -1,81 +1,17 @@
-import * as cache from '@actions/cache'
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
-import * as io from '@actions/io'
-import Docskerode from 'dockerode'
-import fs from 'fs'
-import {
-  BUILDKIT_STATE_PATH,
-  STATE_RESTORED_CACHE_KEY,
-  STATE_TYPES,
-  getContainerName
-} from './common'
-import path from 'path'
+import {version} from '../package.json'
+import {binaryName, getBinary} from './common'
 
 async function run(): Promise<void> {
   try {
-    const cacheKey = core.getInput('cache-key')
-    const restoredCacheKey = core.getState(STATE_RESTORED_CACHE_KEY)
-    if (restoredCacheKey === cacheKey) {
-      core.info('Cache key matched. Ignore cache saving.')
-      return
-    }
-
-    await core.group('Removing unwanted caches', async () => {
-      const targetTypes = core.getMultilineInput('target-types')
-      await Promise.all(
-        STATE_TYPES.filter(type => !targetTypes.includes(type)).map(
-          async type =>
-            exec.exec('docker', [
-              'buildx',
-              'prune',
-              '--force',
-              '--filter',
-              `type=${type}`
-            ])
-        )
-      )
+    core.debug(`version: ${version}`)
+    await core.group('Get binary', async () => {
+      const toolPath = await getBinary(version)
+      core.addPath(toolPath)
     })
 
-    await core.group('Buildx dist usage', async () => {
-      await exec.getExecOutput('docker', ['buildx', 'du', '--verbose'])
-    })
-
-    await core.group('Stopping buildx', async () => {
-      await exec.exec('docker', ['buildx', 'stop'])
-    })
-
-    await core.group('Fetching buildx state', async () => {
-      const buildxName = core.getInput('buildx-name')
-      const buildxContainerName = core.getInput('buildx-container-name')
-
-      const docker = new Docskerode()
-      const container = docker.getContainer(
-        getContainerName({buildxName, buildxContainerName})
-      )
-      core.info(`found container ${container.id}`)
-
-      core.info('Archiving buildkit state into tar file...')
-      await io.mkdirP(path.dirname(BUILDKIT_STATE_PATH))
-      const outputStream = fs.createWriteStream(BUILDKIT_STATE_PATH, {
-        encoding: 'binary'
-      })
-      const inputStream = await container.getArchive({
-        path: '/var/lib/buildkit'
-      })
-      const promiseExecute = async (): Promise<void> => {
-        return new Promise(resolve => {
-          inputStream.pipe(outputStream)
-          outputStream.on('finish', resolve)
-        })
-      }
-      await promiseExecute()
-      outputStream.close()
-    })
-
-    await core.group('Upload into Github cache', async () => {
-      await cache.saveCache([BUILDKIT_STATE_PATH], cacheKey)
-    })
+    await exec.exec(binaryName, ['save'])
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
